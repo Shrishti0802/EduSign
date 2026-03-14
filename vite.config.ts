52e031b70a8ab41fdb275ea8e03817a4331de933
Shrishti R Kundar <shrishtikunder2006@gmail.com>
2026-03-14 08:34:03 +0530

    fix: increase PWA cache size limit for videos

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,mp4,task,wasm}'],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MiB
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/i,
